332c54ed23f7e417df6ae6436520333eefa59d78
Narmada Radhika J S <130883646+narmada-radhika@users.noreply.github.com>
2024-10-20 12:33:22 +0530

    Create fibonacci series.js
    The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. This assignment involves creating a simple web application that generates and displays the Fibonacci series up to a user-specified number.
    
    Key Features:
    
    The user is prompted to enter a number indicating how many terms of the Fibonacci series they want to see.
    
    The application calculates the series dynamically using a loop and displays each term in a structured format.
    
    The program handles invalid inputs by alerting the user.

---
+++ fibonacci series.js
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Fibonacci Series</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <script type="text/javascript">
+    var fib1 = 0, fib2 = 1, fib = 0;
+    var num = prompt("Enter a number : \n", "");
+    if (num != null && num > 0) {
+      document.write("<h1>The first " + num + " numbers in the fiboacci series</h1>");
+      if (num == 1)
+        document.write("<h2>" + fib1 + "</h2>");
+      else {
+        document.write("<h2>" + fib1 + "</h2>");
+        document.write("<h2>" + fib2 + "</h2>");
+      }
+      for (i = 3; i <= num; i++) {
+        fib = fib1 + fib2;
+        document.write("<h2>" + fib + "</h2>");
+        fib1 = fib2;
+        fib2 = fib;
+      }
+    }
+    else
+      alert("<Invalid Input");
+
+  </script>
+</body>
+
+</html>
